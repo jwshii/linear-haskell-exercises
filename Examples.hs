bafd1e1fd2768745c046c1864c166e0ddad9ecad
Jessica Shi <jessica.shi.4955@gmail.com>
2026-04-13 22:55:08 -0400

    make small fix

--- Examples.hs
+++ Examples.hs
@@ -5,8 +5,8 @@ import Prelude.Linear
 
 -- this should compile as-is
 f :: Int %1-> Int
-f x = 0 + x 
+f x = 0 + x
 
 -- if uncommented, this function should not compile
-f :: Int %1-> Int
-f x = x + x
+-- g :: Int %1-> Int
+-- g x = x + x
